2ec9bac5ef286e4eef3a86e660680c0c2ab392c5
szirakibalint <blntsziraki@gmail.com>
2021-08-04 15:38:38 +0200

    update files for deploy

--- data_manager.py
+++ data_manager.py
@@ -3,43 +3,10 @@ import psycopg2
 import psycopg2.extras
 
 
-def establish_connection(connection_data=None):
-    """
-    Create a database connection based on the :connection_data: parameter
-    :connection_data: Connection string attributes
-    :returns: psycopg2.connection
-    """
-    if connection_data is None:
-        connection_data = get_connection_data()
-    try:
-        connect_str = "dbname={} user={} host={} password={}".format(connection_data['dbname'],
-                                                                     connection_data['user'],
-                                                                     connection_data['host'],
-                                                                     connection_data['password'])
-        conn = psycopg2.connect(connect_str)
-        conn.autocommit = True
-    except psycopg2.DatabaseError as e:
-        print("Cannot connect to database.")
-        print(e)
-    else:
-        return conn
-
-
-def get_connection_data(db_name=None):
-    """
-    Give back a properly formatted dictionary based on the environment variables values which are started
-    with :MY__PSQL_: prefix
-    :db_name: optional parameter. By default it uses the environment variable value.
-    """
-    if db_name is None:
-        db_name = os.environ.get('MY_PSQL_DBNAME')
-
-    return {
-        'dbname': db_name,
-        'user': os.environ.get('MY_PSQL_USER'),
-        'host': os.environ.get('MY_PSQL_HOST'),
-        'password': os.environ.get('MY_PSQL_PASSWORD')
-    }
+def establish_connection():
+    connection_string = os.environ.get('DATABASE_URL')
+    connection = psycopg2.connect(connection_string)
+    return connection
 
 
 def execute_statement(statement, variables=None, fetchall=True, select=True):
